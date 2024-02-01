ac9c191c62425729f8357aaec2fcd1c07dbdeb4f
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-01 11:52:02 +0530

    found out that presentation=modal doesn't work for android as it doesn't have a default pop up as ios

--- airbnb/app/_layout.tsx
+++ airbnb/app/_layout.tsx
@@ -1,11 +1,8 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -49,7 +46,37 @@ function RootLayoutNav() {
 
   return (
       <Stack>
+
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+
+        {/* <Stack.Screen
+        name="home"
+        options={{
+          // Hide the header for all other routes.
+          headerShown: false,
+        }}
+      /> */}
+
+
+
+        <Stack.Screen name="(modals)/login" options={{ 
+          presentation: 'modal',
+          title: 'Log in or sign up',
+        }} />
+
+
+
+              {/* <Stack.Screen
+        name="modal"
+        options={{
+          // Set the presentation mode to modal for our modal route.
+          presentation: 'modal',
+        }}
+      /> */}
+
+
+      
       </Stack>
   );
 }
