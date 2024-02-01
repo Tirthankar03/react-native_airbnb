4801f77eb7b9fc0b430ecd97557d991a808b68e0
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-01 06:49:10 +0530

    initial setup of tabs with custom fonts and routing created

--- airbnb/app/(tabs)/_layout.tsx
+++ airbnb/app/(tabs)/_layout.tsx
@@ -2,16 +2,66 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "@/constants/Colors";
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons} from "@expo/vector-icons";
 
 const Layout = () => {
   return (
     <Tabs screenOptions={{
-      tabBarActiveTintColor: Colors.primary
+      tabBarActiveTintColor: Colors.primary,
+      tabBarLabelStyle: {
+        fontFamily: 'mon-sb'
+      }
+
     }}>
       <Tabs.Screen
         name="index"
-        options={{ tabBarLabel: "Explore" }}
+        options={{ tabBarLabel: "Explore",
+        tabBarIcon: ({color, size}) =>
+        <Ionicons name="search" color={color} size={size}/>
+      }}
       ></Tabs.Screen>
+      
+      <Tabs.Screen
+        name="wishlist"
+        options={{ tabBarLabel: "Wishlists",
+        tabBarIcon: ({color, size}) =>
+        <Ionicons name="heart-outline" color={color} size={size}/>
+      }}
+      ></Tabs.Screen>
+
+
+<Tabs.Screen
+        name="trips"
+        options={{ tabBarLabel: "Wishlists",
+        tabBarIcon: ({color, size}) =>
+        <FontAwesome5 name="airbnb" color={color} size={size}/>
+      }}
+      ></Tabs.Screen>
+
+
+<Tabs.Screen
+        name="inbox"
+        options={{ tabBarLabel: "Inbox",
+        tabBarIcon: ({color, size}) =>
+        <MaterialCommunityIcons name="message-outline" color={color} size={size}/>
+      }}
+      ></Tabs.Screen>
+
+
+
+<Tabs.Screen
+        name="profile"
+        options={{ tabBarLabel: "Profile",
+        tabBarIcon: ({color, size}) =>
+        <Ionicons name="person-circle-outline" color={color} size={size}/>
+      }}
+      ></Tabs.Screen>
+
+
+
+
     </Tabs>
   );
 };
