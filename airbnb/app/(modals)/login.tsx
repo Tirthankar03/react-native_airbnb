f721814554e03450dc50a01761fd42944c5bcdf6
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-02 23:58:21 +0530

    clerk set up login and logout logic initialized custom hook useWarmupBrowser added

--- airbnb/app/(modals)/login.tsx
+++ airbnb/app/(modals)/login.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '@/hooks/useWarmupBrowser'
 
 const Page = () => {
+  useWarmUpBrowser();
   return (
     <View>
       <Text>Login is working </Text>
--- airbnb/app/_layout.tsx
+++ airbnb/app/_layout.tsx
@@ -1,17 +1,40 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+
+const tokenCache = {
+  async getToken(key: string){
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string){
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  }
+}
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -19,9 +42,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    'mon': require('../assets/fonts/Montserrat-Regular.ttf'),
-    'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
-    'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
+    mon: require("../assets/fonts/Montserrat-Regular.ttf"),
+    "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -39,22 +62,71 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={
+    tokenCache
+  }>
+    <RootLayoutNav />
+    </ClerkProvider>
+    )
 }
 
+
+///----------------------------------------------------------
+
+
+
 function RootLayoutNav() {
+  const router = useRouter();
+
+
+
+  const {isLoaded, isSignedIn} = useAuth();
+
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login')
+    }
+  }, [isLoaded])
+
+
+
 
   return (
-      <Stack>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Log in or sign up",
+          headerTitleStyle: {
+            fontFamily: "mon-sb",
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
 
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-        <Stack.Screen name="(modals)/login" options={{ 
-          presentation: 'modal',
-          animation: 'slide_from_bottom',
-          title: 'Log in or sign up',
-        }} />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: "" }} />
 
-      </Stack>
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: "transparentModal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+          animation: 'fade',
+        }}
+      />
+    </Stack>
   );
 }
