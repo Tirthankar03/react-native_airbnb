6fb583db26d93abec3956de57d7bd6384ccda108
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-02 09:29:24 +0530

    bug fix code cleanup

--- airbnb/app/(tabs)/index.tsx
+++ airbnb/app/(tabs)/index.tsx
@@ -6,7 +6,6 @@ const Page = () => {
   return (
     <View>
       <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/home"}>Home</Link>
       <Link href={"/(modals)/booking"}>Booking</Link>
       <Link href={"/listing/1337"}>Listing details</Link>
     </View>
