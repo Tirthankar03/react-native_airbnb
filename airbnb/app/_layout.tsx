34ac85b5fa810a86cd8c88cbaa233a166360d008
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-02 08:43:07 +0530

    applied various fixes but wasn't able to resolve the presentation issue. The semi-workaround seems to be the animation-tweak, doesn't directly fix the issue. unable to get the Jsstack hack working, seems to have type errors and even if skipped, doesn't have any change on the component

--- airbnb/app/_layout.tsx
+++ airbnb/app/_layout.tsx
@@ -2,6 +2,47 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { TransitionPresets } from '@react-navigation/stack';
+// import { StackNavigationOptions } from '@react-navigation/stack';
+
+//-------------------------------------------------------------
+import {
+  // Import the creation function
+  createStackNavigator,
+  // Import the types
+  StackNavigationOptions,
+} from "@react-navigation/stack";
+
+import { withLayoutContext } from "expo-router";
+
+const { Navigator } = createStackNavigator();
+
+// This can be used like `<JsStack />`
+// export const JsStack = withLayoutContext(Navigator);
+
+
+//-------------------------------------------------------------
+
+
+// import {
+//   // Import the creation function
+//   createStackNavigator,
+//   // Import the types
+//   StackNavigationOptions,
+// } from "@react-navigation/stack";
+
+// import { withLayoutContext } from "expo-router";
+
+// const { Navigator } = createStackNavigator();
+
+// This can be used like `<JsStack />`
+export const JsStack = withLayoutContext<
+  StackNavigationOptions,
+  typeof Navigator
+>(Navigator);
+
+
+//-------------------------------------------------------------
 
 
 export {
@@ -50,30 +91,32 @@ function RootLayoutNav() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
 
-        {/* <Stack.Screen
+        <Stack.Screen
         name="home"
         options={{
           // Hide the header for all other routes.
           headerShown: false,
         }}
-      /> */}
+      />
 
 
 
         <Stack.Screen name="(modals)/login" options={{ 
           presentation: 'modal',
+          animation: 'slide_from_bottom',
           title: 'Log in or sign up',
         }} />
 
 
 
-              {/* <Stack.Screen
+              <JsStack.Screen
         name="modal"
         options={{
           // Set the presentation mode to modal for our modal route.
           presentation: 'modal',
-        }}
-      /> */}
+          ...TransitionPresets.ModalPresentationIOS,
+        } }
+      />
 
 
       
