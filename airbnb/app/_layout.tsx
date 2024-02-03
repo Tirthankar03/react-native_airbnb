406de3b9af2c815aed6283b98b6d4b5fb96d7b81
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-03 09:13:05 +0530

    basic details added

--- airbnb/app/_layout.tsx
+++ airbnb/app/_layout.tsx
@@ -113,7 +113,7 @@ function RootLayoutNav() {
       />
 
 
-      <Stack.Screen name="listing/[id]" options={{ headerTitle: "" }} />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: "", headerTransparent: true }} />
 
       <Stack.Screen
         name="(modals)/booking"
