b701afaeafec53c86908d09f52e69d4da0b8717b
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-03 08:42:33 +0530

    reanimated used while rendering listings

--- airbnb/components/Listings.tsx
+++ airbnb/components/Listings.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { defaultStyles } from "@/constants/Styles";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, { FadeInRight, FadeOutLeft } from 'react-native-reanimated'
 
 interface Props {
   listings: any[];
@@ -24,35 +25,27 @@ const Listings = ({ listings: items, category }: Props) => {
     }, 200);
   }, [category]);
 
-  const renderRow: ListRenderItem<any> = ({item}) => (
+  const renderRow: ListRenderItem<any> = ({ item }) => (
     <Link href={`/listing/${item.id}`} asChild>
-      <TouchableOpacity style={styles.listing}>
-        <View>
-          <Image source={{uri: item.medium_url}} style={styles.image}/>
-          <TouchableOpacity style={{position: 'absolute', right: 30, top:30}}>
-            <Ionicons name="heart-outline" size={24} color={'#000'}/>
-          </TouchableOpacity>
-        </View>
-
-
-
-        <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
-          <Text style={{fontSize:16, fontFamily: 'mon-sb'}}>{item.name}</Text>
-          <View style={{flexDirection:'row', gap: 4}}>
-            <Ionicons name="star" size={16}/>
-            <Text style={{fontFamily: 'mon-sb'}}>{item.review_scores_rating/20}</Text>
+      <TouchableOpacity>
+        <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
+          <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontWeight: '500', fontSize: 16 }}>{item.name}</Text>
+            <View style={{ flexDirection: 'row', gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontWeight: '500' }}>{item.review_scores_rating / 20}</Text>
+            </View>
           </View>
-        </View>
-
-
-                 <Text>{item.room_type}</Text>
+          <Text>{item.room_type}</Text>
           <View style={{ flexDirection: 'row', gap: 4 }}>
             <Text style={{ fontWeight: '500' }}>€ {item.price}</Text>
             <Text>night</Text>
           </View>
+        </Animated.View>
       </TouchableOpacity>
     </Link>
-  )
+  );
 
 
 
@@ -64,8 +57,7 @@ const Listings = ({ listings: items, category }: Props) => {
       <FlatList
       renderItem={renderRow}
       // ref={listRef} 
-      // data={loading ? [] : items} />
-      data={items} />
+      data={loading ? [] : items} />
     </View>
   );
 };
