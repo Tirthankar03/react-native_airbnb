326d7a92fbc30e9f644f39d87e5a7aac853bea26
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-07 10:13:22 +0530

    first 10 markers displayed on map

--- airbnb/components/ListingsMap.tsx
+++ airbnb/components/ListingsMap.tsx
@@ -1,24 +1,62 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import { defaultStyles } from '@/constants/Styles'
+import { useRouter } from 'expo-router';
 
 
 interface Props{
     listings: any,
 }
 
-
+const INITIAL_REGION = {
+    latitude: 26.14,
+    longitude: 91.73,
+    latitudeDelta: 9,
+    longitudeDelta: 9,
+  };
 
 
 const ListingsMap = ({listings}: Props) => {
+
+  const router = useRouter();
+
+    const onMarkerSelected = (item:any) => { 
+        router.push(`/listing/${item.properties.id}`)
+        
+     }
+
+     const firstTenListings = listings.features.slice(0,10);
+
+     console.log("first 10>>>>>>>", firstTenListings);
+     
+
+
   return (
     <View style={defaultStyles.container}>
-    <MapView style={styles.map} 
+    <MapView style={StyleSheet.absoluteFillObject} 
     provider={PROVIDER_GOOGLE}
     showsUserLocation
     showsMyLocationButton
-    />
+    initialRegion={INITIAL_REGION}
+>
+    {firstTenListings.map((item:any) => (
+        <Marker
+        key={item.properties.id}
+        coordinate={{
+            latitude: +item.properties.latitude,
+            longitude: +item.properties.longitude,
+          }}
+        onPress={() => onMarkerSelected(item)}
+          >
+            <View style={styles.marker}>
+              <Text style={styles.markerText}>
+                $ {item.properties.price}
+              </Text>
+            </View>
+          </Marker>
+    ))}
+</MapView>
     </View>
   )
 }
@@ -28,9 +66,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    map: {
-        width: '100%',
-        height: '100%',
+    marker:{
+      backgroundColor: '#fff',
+      borderRadius: 12,
+      padding: 6,
+      // paddingHorizontal: 2,
+      alignItems: 'center',
+      justifyContent:"center",
+      elevation: 5,
+      shadowColor: '#000',
+      shadowOpacity: 0.5,
+      shadowRadius: 7,
+      shadowOffset: {
+        width: 1,
+        height: 10,
+      }
+    },
+    markerText:{
+      fontSize: 14,
+      fontFamily: 'mon-sb'
     }
 })
 
