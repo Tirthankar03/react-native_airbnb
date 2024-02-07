a610139a842df38239af367059c509fea8e3bbb4
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-02-07 10:56:30 +0530

    custom styles for cluster added

--- airbnb/components/ListingsMap.tsx
+++ airbnb/components/ListingsMap.tsx
@@ -1,91 +1,145 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
-import { defaultStyles } from '@/constants/Styles'
-import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { defaultStyles } from "@/constants/Styles";
+import { useRouter } from "expo-router";
+import MapView from "react-native-map-clustering";
 
-
-interface Props{
-    listings: any,
+interface Props {
+  listings: any;
 }
 
 const INITIAL_REGION = {
-    latitude: 26.14,
-    longitude: 91.73,
-    latitudeDelta: 9,
-    longitudeDelta: 9,
+  latitude: 26.14,
+  longitude: 91.73,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
+
+const ListingsMap = ({ listings }: Props) => {
+  const router = useRouter();
+
+  const onMarkerSelected = (item: any) => {
+    router.push(`/listing/${item.properties.id}`);
+  };
+
+  const first20Listings = listings.features.slice(0, 20);
+
+  console.log("first 10>>>>>>>", first20Listings);
+
+  // const renderCluster = (cluster: any) => {
+  //   const { id, geometry, onPress, properties } = cluster;
+  //   const points = properties.point_count;
+
+  //   return (
+  //     <Marker
+  //       key={`cluster-${id}`}
+  //       coordinate={{
+  //         latitude: geometry.coordinates[0],
+  //         longitude: geometry.coordinates[1],
+  //       }}
+  //     >
+  //       <View>
+  //         <Text>Test</Text>
+  //       </View>
+  //     </Marker>
+  //   );
+  // };
+
+
+  const renderCluster = (cluster: any) => {
+    const { id, geometry, onPress, properties } = cluster;
+
+    const points = properties.point_count;
+    return (
+      <Marker
+        key={`cluster-${id}`}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+        onPress={onPress}>
+        <View style={styles.marker}>
+          <Text
+            style={{
+              color: '#000',
+              textAlign: 'center',
+              fontFamily: 'mon-sb',
+            }}>
+            {points}
+          </Text>
+        </View>
+      </Marker>
+    );
   };
 
 
-const ListingsMap = ({listings}: Props) => {
-
-  const router = useRouter();
-
-    const onMarkerSelected = (item:any) => { 
-        router.push(`/listing/${item.properties.id}`)
-        
-     }
-
-     const firstTenListings = listings.features.slice(0,10);
-
-     console.log("first 10>>>>>>>", firstTenListings);
-     
 
 
+
+
+
+
+
+
+  
   return (
     <View style={defaultStyles.container}>
-    <MapView style={StyleSheet.absoluteFillObject} 
-    provider={PROVIDER_GOOGLE}
-    showsUserLocation
-    showsMyLocationButton
-    initialRegion={INITIAL_REGION}
->
-    {firstTenListings.map((item:any) => (
-        <Marker
-        key={item.properties.id}
-        coordinate={{
-            latitude: +item.properties.latitude,
-            longitude: +item.properties.longitude,
-          }}
-        onPress={() => onMarkerSelected(item)}
+      <MapView
+        style={StyleSheet.absoluteFillObject}
+        animationEnabled={false}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        initialRegion={INITIAL_REGION}
+        clusterColor="#fff"
+        clusterTextColor="#000"
+        clusterFontFamily="mon-sb"
+        renderCluster={renderCluster}
+      >
+        {first20Listings.map((item: any) => (
+          <Marker
+            key={item.properties.id}
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+            onPress={() => onMarkerSelected(item)}
           >
             <View style={styles.marker}>
-              <Text style={styles.markerText}>
-                $ {item.properties.price}
-              </Text>
+              <Text style={styles.markerText}>$ {item.properties.price}</Text>
             </View>
           </Marker>
-    ))}
-</MapView>
+        ))}
+      </MapView>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+  container: {
+    flex: 1,
+  },
+  marker: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    padding: 6,
+    // paddingHorizontal: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.5,
+    shadowRadius: 7,
+    shadowOffset: {
+      width: 1,
+      height: 10,
     },
-    marker:{
-      backgroundColor: '#fff',
-      borderRadius: 12,
-      padding: 6,
-      // paddingHorizontal: 2,
-      alignItems: 'center',
-      justifyContent:"center",
-      elevation: 5,
-      shadowColor: '#000',
-      shadowOpacity: 0.5,
-      shadowRadius: 7,
-      shadowOffset: {
-        width: 1,
-        height: 10,
-      }
-    },
-    markerText:{
-      fontSize: 14,
-      fontFamily: 'mon-sb'
-    }
-})
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+  },
+});
 
-export default ListingsMap
+export default ListingsMap;
